963d396f964f0ae6ff8283def80066a32489ac07
Diabeetus <t76rykyfuk6jruymhgf675iverj@gmodsc.net>
2017-02-05 13:28:10 -0500

    Fixed Typo
    Fixed the typo. This update is not recommended as the script works fine
    without the update. This is just a type fix.

--- lua/autorun/lwcars_bmw850csi.lua
+++ lua/autorun/lwcars_bmw850csi.lua
@@ -15,6 +15,6 @@ list.Set("Vehicles", "bmw_850csi_lw", V)
 if SERVER then include("lwcars_partmover.lua")
 
 	LWCPartHook( 0.02, "pop_ups", "bmw_850csi", IN_ATTACK, string.lower(V.Model) ) --0.02
-	LWCPartHook( 0.005, "sunroof", "bmw_850c5si", IN_ATTACK2, string.lower(V.Model) ) --0.005
+	LWCPartHook( 0.005, "sunroof", "bmw_850csi", IN_ATTACK2, string.lower(V.Model) ) --0.005
 	
 end
